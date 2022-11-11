8f2b19642f98563979df0df953f4765d85b58531
walid-hrizi <walidhrizi5@gmail.com>
2022-11-11 10:48:33 +0100

    corn job

--- controllers/cron.js
+++ controllers/cron.js
@@ -1,6 +1,4 @@
 //Implement CRON JOB to check “ air quality “ for the Paris zone ( latitude: 48.856613 ,longitude: 2.352222)
-
-const open = require('open');
 var cron = require('node-cron');
 const http = require('http');
 cron.schedule('* * * * *', () => {
